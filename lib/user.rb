7be41b6bf46728188a915bc609849ea817f7d215
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-22 13:33:24 +0200

    Add files via upload

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class User
 
+attr_accessor :email, :age
+@@user_count = 0
+@@all_users = []
+
+def initialize(email_to_save, age_to_save)
+	@email = email_to_save
+	@age = age_to_save
+	@@all_users 
+
+end 
+
+
+def self.all
+	return @@all_users
+	
+end 
+
 end
+
+binding.pry
+
+puts "end of file"
